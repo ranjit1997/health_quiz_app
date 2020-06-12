53025fe50a61bd2e75227ccfa56d7726ddc55bb7
ranjit1997 <44993852+ranjit1997@users.noreply.github.com>
2020-06-13 00:18:53 +0530

    final commit

---
+++ lib/utility/api_provider.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:heath_quiz_app/models/category.dart';
+import 'package:heath_quiz_app/models/question.dart';
+import 'package:http/http.dart' as http;
+
+const String baseUrl = "https://opentdb.com/api.php";
+
+Future<List<Question>> getQuestions(Category category, int total, String difficulty) async {
+  String url = "$baseUrl?amount=$total&category=${category.id}";
+  if(difficulty != null) {
+    url = "$url&difficulty=$difficulty";
+  }
+  http.Response res = await http.get(url);
+  List<Map<String, dynamic>> questions = List<Map<String,dynamic>>.from(json.decode(res.body)["results"]);
+  return Question.fromData(questions);
+}
